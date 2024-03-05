57e29e237e3be4f219e90d5479fcbefabb200773
Konstantin Gasser <konstantin.gasser@me.com>
2024-03-05 16:26:30 +0100

    rust: dynamic board size

--- rust/tic_tac_toe/src/main.rs
+++ rust/tic_tac_toe/src/main.rs
@@ -18,14 +18,14 @@ enum GameError {
 
 enum BoardOpt {
     ThreeByThree = 3,
-    NineByNine = 9,
+    FourByFour = 4,
 }
 
 impl BoardOpt {
     fn dim(self) -> usize {
         match self {
             BoardOpt::ThreeByThree => 3 * 3,
-            BoardOpt::NineByNine => 9 * 9,
+            BoardOpt::FourByFour => 4 * 4,
         }
     }
 }
@@ -58,39 +58,56 @@ impl Game {
     fn to_1d_index(&self, x: usize, y: usize) -> usize {
         match self.state.len() {
             9 => x * 3 + y,
-            27 => x * 9 + y,
-            _ => unreachable!("other board options besides (3x3 and 9x9) should not be allowed"),
+            16 => x * 4 + y,
+            _ => unreachable!("other board options besides (3x3 and 4x4) should not be allowed"),
         }
     }
 }
 
 impl fmt::Display for Game {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        writeln!(f, " - - -").unwrap();
-        self.state
-            .chunks(BoardOpt::ThreeByThree as usize)
-            .for_each(|row| {
-                row.iter().for_each(|cell| {
-                    let char = match cell {
-                        Cell::Taken(player) => match player {
-                            Player::Cross => "X".to_string(),
-                            Player::Circle => "O".to_string(),
-                        },
-                        Cell::Free => " ".to_string(),
-                    };
+        let mut divider = |f: &mut fmt::Formatter, count: usize| {
+            writeln!(f).unwrap();
+            for _i in 0..count {
+                write!(f, " ---").unwrap();
+            }
+            writeln!(f).unwrap();
+        };
 
-                    write!(f, "|{}", char).unwrap();
-                });
-                writeln!(f, "|\n - - -").unwrap();
+        let chunk_size: usize = match self.state.len() {
+            9 => 3,
+            16 => 4,
+            _ => unreachable!("other board options besides (3x3 and 4x4) should not be allowed"),
+        };
+
+        divider(f, chunk_size);
+
+        self.state.chunks(chunk_size).for_each(|row| {
+            row.iter().for_each(|cell| {
+                let char = match cell {
+                    Cell::Taken(player) => match player {
+                        Player::Cross => " X ".to_string(),
+                        Player::Circle => " O ".to_string(),
+                    },
+                    Cell::Free => "   ".to_string(),
+                };
+
+                write!(f, "|{}", char).unwrap();
             });
+            write!(f, "|").unwrap();
+            divider(f, chunk_size);
+        });
 
         Ok(())
     }
 }
 
 fn main() {
-    let mut game = Game::new(BoardOpt::ThreeByThree);
+    let mut game = Game::new(BoardOpt::FourByFour);
 
     let _ = game.apply(1, 1);
+    println!("{game}");
+
+    let _ = game.apply(0, 1);
     println!("{game}");
 }
