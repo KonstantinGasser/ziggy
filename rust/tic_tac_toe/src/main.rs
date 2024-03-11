71521cca851426bd145342c5935779c5fd4bea13
Konstantin Gasser <konstantin.gasser@me.com>
2024-03-11 15:12:12 +0100

    rust: tic-tac-toe: feat: check if player won, check that cell not overwritten

--- rust/tic_tac_toe/src/main.rs
+++ rust/tic_tac_toe/src/main.rs
@@ -1,6 +1,6 @@
 use std::{error::Error, fmt, io, usize};
 
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Copy, Debug, Clone, PartialEq)]
 enum Player {
     Cross,
     Circle,
@@ -9,6 +9,7 @@ enum Player {
 #[derive(Debug)]
 enum GameError {
     InvalidSize(String),
+    FieldNotFree,
 }
 
 enum BoardOpt {
@@ -25,6 +26,7 @@ impl BoardOpt {
     }
 }
 
+#[derive(Debug)]
 enum State {
     Running,
     Tie,
@@ -32,7 +34,7 @@ enum State {
 }
 
 struct Game {
-    state: Vec<Option<Player>>, // game board
+    state: Vec<Option<Player>>, // game self.state
     turn: Player,
 }
 
@@ -47,7 +49,10 @@ impl Game {
     fn apply(&mut self, placement: Placement) -> Result<State, GameError> {
         let index = self.to_1d_index(placement.0, placement.1);
 
-        self.state[index] = Some(self.turn.clone());
+        if self.state[index].is_some() {
+            return Err(GameError::FieldNotFree);
+        }
+        self.state[index] = Some(self.turn);
 
         // check if state if winner and if so map ro Result<Winner>
         match self.current_state() {
@@ -64,21 +69,89 @@ impl Game {
     }
 
     fn current_state(&self) -> Option<State> {
-        Some(State::Running)
+        let size: usize = match self.state.len() {
+            9 => 3,
+            16 => 4,
+            _ => unreachable!("other game modes besized 3x3 and 4x4 do not exist"),
+        };
+
+        // Check rows
+        for i in (0..size.pow(2)).step_by(size) {
+            if let Some(player) = self.state[i] {
+                let mut is_winner = true;
+                for j in 1..size {
+                    if self.state[i + j] != Some(player) {
+                        is_winner = false;
+                        break;
+                    }
+                }
+                if is_winner {
+                    return Some(State::Winner(player));
+                }
+            }
+        }
+
+        // Check columns
+        for i in 0..size {
+            if let Some(player) = self.state[i] {
+                let mut is_winner = true;
+                for j in 1..size {
+                    if self.state[i + j * size] != Some(player) {
+                        is_winner = false;
+                        break;
+                    }
+                }
+                if is_winner {
+                    return Some(State::Winner(player));
+                }
+            }
+        }
+
+        // Check diagonals
+        let mut is_winner = true;
+        if let Some(player) = self.state[0] {
+            for i in 1..size {
+                if self.state[i * size + i] != Some(player) {
+                    is_winner = false;
+                    break;
+                }
+            }
+            if is_winner {
+                return Some(State::Winner(player));
+            }
+        }
+
+        is_winner = true;
+        if let Some(player) = self.state[size - 1] {
+            for i in 1..size {
+                if self.state[(i + 1) * (size - 1)] != Some(player) {
+                    is_winner = false;
+                    break;
+                }
+            }
+            if is_winner {
+                return Some(State::Winner(player));
+            }
+        }
+
+        // No winner yet
+        None
     }
 
     fn to_1d_index(&self, x: usize, y: usize) -> usize {
         match self.state.len() {
             9 => x * 3 + y,
             16 => x * 4 + y,
-            _ => unreachable!("other board options besides (3x3 and 4x4) should not be allowed"),
+            _ => {
+                unreachable!("other self.state options besides (3x3 and 4x4) should not be allowed")
+            }
         }
     }
 }
 
 impl fmt::Display for Game {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut divider = |f: &mut fmt::Formatter, count: usize| {
+        let divider = |f: &mut fmt::Formatter, count: usize| {
             writeln!(f).unwrap();
             for _i in 0..count {
                 write!(f, " ---").unwrap();
@@ -89,7 +162,9 @@ impl fmt::Display for Game {
         let chunk_size: usize = match self.state.len() {
             9 => 3,
             16 => 4,
-            _ => unreachable!("other board options besides (3x3 and 4x4) should not be allowed"),
+            _ => {
+                unreachable!("other self.state options besides (3x3 and 4x4) should not be allowed")
+            }
         };
 
         divider(f, chunk_size);
@@ -151,11 +226,12 @@ fn main() {
         buffer.clear(); // is there a safer way of cleaning it up? while resuing the same
                         // underlying buffer?
 
-        // let _ = game.apply(placement);
-        if let Ok(State::Winner(player)) = game.apply(placement) {
-            println!("Player {:?} won the game!", player);
-        }
-        // clear_screen();
+        match game.apply(placement) {
+            Ok(State::Winner(player)) => println!("Player {:?} won the game!", player),
+            Err(err) => println!("Error: {:?}", err),
+            _ => (),
+        };
+
         println!("{game}");
     }
 }
