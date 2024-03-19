a62a05a1100967a30ba9fc702d8ccc965c570728
Konstantin Gasser <konstantin.gasser@me.com>
2024-03-19 08:42:20 +0100

    rust: http: counter: placing error close to source we can display error if Some and by reswap on next request error message is re-evaluated

--- rust/http/counter/src/handlers/index.rs
+++ rust/http/counter/src/handlers/index.rs
@@ -10,6 +10,7 @@ use crate::handlers::html_render;
 #[template(path = "base.html")]
 pub struct BaseTemplate {
     view_counter: usize,
+    error: Option<String>,
 }
 
 #[derive(Template)]
@@ -29,6 +30,7 @@ pub async fn home(Extension(state): axum::extract::Extension<app::App>) -> impl 
     let count = state.get_count();
     html_render::TemplateResponse(BaseTemplate {
         view_counter: count,
+        error: None,
     })
 }
 pub async fn increment(Extension(state): axum::extract::Extension<app::App>) -> impl IntoResponse {
@@ -42,14 +44,19 @@ pub async fn increment(Extension(state): axum::extract::Extension<app::App>) -> 
 
 pub async fn decrement(Extension(state): axum::extract::Extension<app::App>) -> impl IntoResponse {
     let Some(new_count) = state.decrement() else {
-        let mut resp = html_render::TemplateResponse(ErrorTemplate {
-            error: "negative count not allowed".to_owned(),
+        return html_render::TemplateResponse(CounterTemplate {
+            view_counter: state.get_count(),
+            error: Some("negative count not allowed".to_owned()),
         })
         .into_response();
-
-        resp.headers_mut()
-            .insert("HX-Retarget", HeaderValue::from_static("#error"));
-        return resp;
+        // let mut resp = html_render::TemplateResponse(ErrorTemplate {
+        //     error: "negative count not allowed".to_owned(),
+        // })
+        // .into_response();
+        //
+        // resp.headers_mut()
+        //     .insert("HX-Retarget", HeaderValue::from_static("#error"));
+        // return resp;
     };
 
     html_render::TemplateResponse(CounterTemplate {
