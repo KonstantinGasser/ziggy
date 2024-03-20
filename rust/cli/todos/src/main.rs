3e53c464f264dfc52a6788e34762a4aacec75caf
Konstantin Gasser <konstantin.gasser@me.com>
2024-03-20 09:37:24 +0100

    rust: cli: todos: add subcommand with command flags

--- rust/cli/todos/src/main.rs
+++ rust/cli/todos/src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::Context;
 use clap::{Parser, Subcommand};
 
 mod todo;
@@ -12,6 +13,10 @@ struct Args {
 #[derive(Subcommand, Debug)]
 enum Command {
     List,
+    Create {
+        #[clap(short = 't', long)]
+        title: String,
+    },
 }
 
 const CONFIG_PATH: &'static str = "todos.json";
@@ -19,13 +24,19 @@ const CONFIG_PATH: &'static str = "todos.json";
 fn main() {
     let args = Args::parse();
 
-    let store = match todo::store::Store::from_fs(CONFIG_PATH) {
+    let mut store = match todo::store::Store::from_fs(CONFIG_PATH) {
         Ok(store) => store,
         Err(err) => panic!("Initialization of CLI failed: {err}"),
     };
 
     match args.command {
         Command::List => println!("List of ToDos:\n\n{}", store.format_todos()),
+        Command::Create { title } => {
+            let td = todo::store::ToDo::new(title);
+            store.add_todo(td);
+
+            let _ = store.write_fs(CONFIG_PATH).context("write created ToDo");
+        }
     };
 }
 
--- rust/cli/todos/src/todo/store.rs
+++ rust/cli/todos/src/todo/store.rs
@@ -1,17 +1,28 @@
 use anyhow::Context;
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 pub struct Store {
-    // what does here??
     items: Vec<ToDo>,
 }
 
 #[derive(Deserialize, Serialize)]
 pub struct ToDo {
-    id: usize,
+    id: String,
     title: String,
     slug: String,
-    done: Option<()>,
+    done: bool,
+}
+
+impl ToDo {
+    pub fn new(title: String) -> Self {
+        ToDo {
+            id: Uuid::new_v4().to_string(),
+            title: title,
+            slug: String::from("Mhm, what can go here?"),
+            done: false,
+        }
+    }
 }
 
 impl Store {
@@ -19,19 +30,38 @@ impl Store {
         let raw = std::fs::read_to_string(location)
             .with_context(|| format!("read todo json file: {location}"))?;
 
-        let todos: Vec<ToDo> = serde_json::from_str(&raw).context("parse todo json file")?;
+        let todos = serde_json::from_str::<Vec<ToDo>>(&raw).context("parse todo json file")?;
 
         Ok(Store { items: todos })
+    }
+
+    // NOTE: adding a todo does not automatically save the todos to local storage.
+    // this needs to be done in a following step.
+    pub fn add_todo(&mut self, td: ToDo) {
+        self.items.push(td);
+    }
+
+    pub fn write_fs(&self, location: &str) -> Result<(), anyhow::Error> {
+        let pretty_bytes =
+            serde_json::to_vec_pretty(&self.items).context("write ToDo items to local storage")?;
+
+        std::fs::write(location, pretty_bytes.as_slice())
+            .with_context(|| format!("write ToDo items to json file: {location}"))?;
+        Ok(())
     }
 
     pub fn format_todos(&self) -> String {
         if self.items.is_empty() {
             return "No ToDos found - have a lovely day!".to_string();
         }
+
         let mut out = String::new();
-        self.items
-            .iter()
-            .for_each(|item| out.push_str(&format!("ID: {} -- Title: {}", item.id, &item.title)));
+        self.items.iter().for_each(|item| {
+            out.push_str(&format!(
+                "ID: {} -- Title: {} -- Done: {}",
+                item.id, &item.title, item.done
+            ))
+        });
 
         out
     }
